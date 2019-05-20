9671d999614272b5617bfa795d64f14ba1766b9d
zeko.byeon <zeko.byeon@cheilpengtai.com>
2019-05-20 18:47:49 +0900

    react study test

---
+++ src/components-test/PageTemplate/PageTemplate.js
@@ -0,0 +1,15 @@
+import React, { } from "react";
+import "./PageTemplate.scss";
+
+const PageTemplate = ({children}) => {
+	return(
+		<div className="page-template">
+			<h1>일정 관리</h1>
+			<div className="content">
+				{children}
+			</div>
+		</div>
+	)
+}
+
+export default PageTemplate;
